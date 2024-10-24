0989bc457cd751fa4790037872b0cac628b92a8e
Vijay Suthar <>
2024-10-24 12:28:49 +0530

    development: Remove personal creds

--- notification-service/src/providers/sms.ts
+++ notification-service/src/providers/sms.ts
@@ -11,7 +11,7 @@ export const SendVerificationCode = async (
 ) => {
     const response = await client.messages.create({
         body: `Your verification code is ${code} it will expire within 30 minutes.`,
-        from: "+15738594262",
+        from: "+91-XXXXXXXXXX",
         to: toPhoneNumber.trim(),
     });
     console.log(response);
